2ee03310d1fb3d57acd28f78d6fedf4cc2c2d1e1
davbrican <72500941+davbrican@users.noreply.github.com>
2022-02-28 19:54:33 +0100

    checkFiles

--- PAI1/index.js
+++ PAI1/index.js
@@ -1,21 +1,55 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const main = () => {
-    // Function to check file integrity
-    checkIntegrity(__dirname + '/files/csv0.csv');
+var filesArray = [];
+const filesPath = './files/'
 
-}
-
-const checkIntegrity = (file) => {
-
+function createHash(file) {
     const fileBuffer = fs.readFileSync(file);
     const hashSum = crypto.createHash('sha256');
     hashSum.update(fileBuffer);
 
     const hex = hashSum.digest('hex');
 
-    console.log(hex);
+    return hex;
 }
+
+async function storeFiles() {
+    let filesArr = [];
+    return new Promise((resolve, reject) => {
+        fs.readdir(filesPath, (err, files) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            } else {
+                files.forEach(file => {
+                    filesArr.push({"path": file, "hash": createHash(filesPath + file)});
+                });
+                return resolve(filesArr);
+            }
+        });
+    })
+}
+
+const main = async () => {
+    // Create an array of files
+    const filesArray = await storeFiles();
+    
+    // Function to check file integrity
+    filesArray.forEach(file => {
+        checkIntegrity(file);
+    });
+}
+
+const checkIntegrity = (file) => {
+    const hash = createHash(filesPath+file.path)
+    if (hash === file.hash) {
+        console.log(`${file.path} is OK`);
+    } else {
+        console.log(`${file.path} is corrupted`);
+    }
+}
+
+
 
 main();
