ba761a52ad49d5e24a23c5f68317efa83b7cc469
davbrican <72500941+davbrican@users.noreply.github.com>
2022-02-28 20:23:59 +0100

    yepa que pasa gente

--- PAI1/index.js
+++ PAI1/index.js
@@ -1,9 +1,14 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-var filesArray = [];
 const filesPath = './files/'
 
+// Function to generate random number
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min));
+}
+
+//  Function to create hash
 function createHash(file) {
     const fileBuffer = fs.readFileSync(file);
     const hashSum = crypto.createHash('sha256');
@@ -14,6 +19,7 @@ function createHash(file) {
     return hex;
 }
 
+//  Function to store files in an array
 async function storeFiles() {
     let filesArr = [];
     return new Promise((resolve, reject) => {
@@ -31,25 +37,73 @@ async function storeFiles() {
     })
 }
 
-const main = async () => {
-    // Create an array of files
-    const filesArray = await storeFiles();
-    
-    // Function to check file integrity
-    filesArray.forEach(file => {
-        checkIntegrity(file);
-    });
-}
-
+// Function to check file integrity
 const checkIntegrity = (file) => {
     const hash = createHash(filesPath+file.path)
     if (hash === file.hash) {
         console.log(`${file.path} is OK`);
     } else {
         console.log(`${file.path} is corrupted`);
+        // TODO Restore file
     }
+}
+
+// Function to corrupt a random file
+function corruptRandomFile(file) {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const random = randomNumber(0, data.length);
+            data[random] = randomNumber(0, 255);
+            fs.writeFile(file, data, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`${file} is corrupted`);
+                }
+            });
+        }
+    });
 }
 
 
 
-main();
+
+//  main function
+const main = async () => {
+    // Create an array of files
+    const filesArray = await storeFiles();
+    
+
+    // Check integrity of each file each second
+    var interval = setInterval(function () {
+        console.log("\n\nChecking integrity of file: " + filesPath);
+        filesArray.forEach(file => {
+            checkIntegrity(file);
+        })
+    }, 1000);
+    
+    // Random time for interval2
+    var randomTime = randomNumber(1500, 3000);
+    // Corrupt a random file each randomTime seconds
+    var interval2 = setInterval(function () {
+        var file = filesPath + filesArray[randomNumber(0, filesArray.length)].path;
+        console.log("\n\nCorrupting file: " + file);
+        corruptRandomFile(file);
+    }, randomTime);
+
+
+    // Stop both intervals after 10 seconds
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 10000);
+    
+    setTimeout(() => {
+        clearInterval(interval2);
+    }, 10000);
+}
+
+
+
+main(); // TODO Crear TXT con la información de la consola con timestamp y otro archivo CSV con información numérica de lo que ocurre (Nº archivos OK, Nº de archivos corruptos, Nº de archivos corruptos, etc.)
