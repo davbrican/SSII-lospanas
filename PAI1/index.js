5f6741af57901a18e53e9ce26faa4b9b39b71253
davbrican <72500941+davbrican@users.noreply.github.com>
2022-03-08 21:13:15 +0100

    yepa kpazagnt

--- PAI1/index.js
+++ PAI1/index.js
@@ -5,6 +5,7 @@ var nodemailer = require('nodemailer');
 const { send, env } = require('process');
 require('dotenv').config();
 const createCSV = require('csv-writer').createObjectCsvWriter;
+const readline = require('readline');
 
 const filesPath = './files/';
 var nonces = []
@@ -17,10 +18,11 @@ var challenges = {
 }
 
 // Variables que establece el usuario al ejecutar el script
-const sendMailYesNo = true;
+var sendMailYesNo = false;
+var hashInput = 0;
 const hashType = ['sha256', 'sha512', 'sha384'];
 const secret = 'clave-simetrica-secreta';
-
+var correoDst = "";
 // Function to generate random number
 function randomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -29,7 +31,7 @@ function randomNumber(min, max) {
 //  Function to create hash
 function createHash(file, nonce) {
     let fileBuffer = fs.readFileSync(file);
-    const hashSum = crypto.createHash(hashType[0]);
+    const hashSum = crypto.createHash(hashType[hashInput]);
 
     if(nonce){
         fileBuffer = addNonceToFile(fileBuffer,nonce);
@@ -60,7 +62,7 @@ function createHmac(file, token) {
     const fileBuffer = fs.readFileSync(file);
     const challenge = challenges[token](fileBuffer);
 
-    const hmac = crypto.createHmac(hashType[0], secret);
+    const hmac = crypto.createHmac(hashType[hashInput], secret);
     
     hmac.update(challenge);
 
@@ -74,7 +76,7 @@ async function storeFiles() {
     return new Promise((resolve, reject) => {
         fs.readdir(filesPath, (err, files) => {
             if (err) {
-                console.log(err);
+                //console.log(err);
                 return reject(err);
             } else {
                 files.forEach(file => {
@@ -94,7 +96,7 @@ function replaceContents(file, replacement, cb) {
         if (err) return cb(err);
         fs.writeFile(file, contents, cb);
     });
-    console.log("Restoring contents of file: " + file);
+    //console.log("Restoring contents of file: " + file);
     return "\nRestoring contents of file: " + file;
 }
 
@@ -103,19 +105,19 @@ const checkIntegrity = async (file) => {
     const hash = createHash(filesPath+file.path);
     var text = "";
     if (hash === file.hash) {
-        console.log(`${file.path} is OK`);
+        //console.log(`${file.path} is OK`);
         text += `\n${file.path} is OK`;
         return [true, text];
     } else {
-        console.log(`${file.path} is corrupted`);
+        //console.log(`${file.path} is corrupted`);
         text += `\n${file.path} is corrupted`;
         if(sendMailYesNo) {
-            //sendMail();
+            sendMail();
         }
         // Restore file
         var restoring = await replaceContents(filesPath + file.path, './backupFiles/' + file.path, err => {
             if (err) {
-                console.log(err);
+                //console.log(err);
             }
         });
         text += restoring;
@@ -128,18 +130,18 @@ const checkIntegrity = async (file) => {
 function corruptRandomFile(file) {
     fs.readFile(file, (err, data) => {
         if (err) {
-            console.log(err);
+            //console.log(err);
         } else {
             const random = randomNumber(0, data.length);
             data[random] = randomNumber(0, 255);
             var texto = "";
             fs.writeFile(file, data, (err) => {
                 if (err) {
-                    console.log(err);
+                    //console.log(err);
                 } else {
-                    console.log(`${file} has been corrupted`);
+                    //console.log(`${file} has been corrupted`);
                     texto += `\n${file} has been corrupted`;
-                    createReport("./checkingReport.txt", `${file} has been corrupted`);
+                    createReport("corruptReport.txt", `${file} has been corrupted`);
                 }
             });
         }
@@ -160,8 +162,8 @@ async function sendMail() {
 
     
     let mailOptions = {
-        from: 'david.brincau@hotmail.com', // sender address
-        to: process.env.TO_MAIL, // list of receivers
+        from: process.env.EMAIL, // sender address
+        to: correoDst, // list of receivers
         subject: "Corrupted file", // Subject line
         text: "A file has been detected to be corrupted while checking system files integrity.", // plain text body
         html: `
@@ -170,26 +172,27 @@ async function sendMail() {
         <p>One of your files is corrupted.</p>`, // html body
       };
       
-    console.log("Sending mail...");
+    //console.log("Sending mail...");
     transporter.sendMail(mailOptions, function(error, info){
         if (error) {
             return console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
+        //console.log('Message sent: %s', info.messageId);
     });
 }
 
 // Function to create a report
 function createReport(reportFile, content) {
+    reportFile = "./Reports/" + reportFile;
     fs.readFile(reportFile, (err, data) => {
         if (err) {
-            console.log(err);
+            //console.log(err);
         } else {
             fs.writeFile(reportFile, data + '\n' + content, (err) => {
                 if (err) {
-                    console.log(err);
+                    //console.log(err);
                 } else {
-                    console.log("\nReport created");
+                    //console.log("\nReport created");
                 }
             });
         }
@@ -201,7 +204,8 @@ function createCSVReport(csv, index, okD, corD) {
     csv.writeRecords([
         { id: index, ok: okD, cor: corD }
     ])
-    .then(() => { console.log("Report created"); });
+    .then(() => { //console.log("Report created"); 
+    });
 }
 
 function checkHmacClient(file,hmac,token){
@@ -216,13 +220,13 @@ function checkHmacClient(file,hmac,token){
 // Function client send file to server
 function proofOfPossesion(file,fileHash,token,nonce) {
     if(nonces.includes(nonce)){
-        console.log("Nonce already used");
+        //console.log("Nonce already used");
         return {success:false,message:"Nonce already used"};
     }
     const hash = createHash(file,nonce);
     if(fileHash === hash){
         nonces.push(nonce);
-        console.log(`${file} is OK`);
+        //console.log(`${file} is OK`);
         hmac = createHmac(file,token);
         const clientCheck = checkHmacClient(file,hmac,token)
         if(clientCheck) return {success:true,hmac:hmac};
@@ -233,15 +237,15 @@ function proofOfPossesion(file,fileHash,token,nonce) {
     }
 }
 
-
-//  main function
-const main = async () => {
+// Function to simulate server behaviuor
+async function serverSimulation() {
+    console.log("\nSimulando servidor...");
     // Create an array of files
     const filesMap = await storeFiles();
     const keysArray = Array.from(filesMap.keys());
     
     var csv = createCSV({
-        path: "dataReport.csv",
+        path: "./Reports/dataReport.csv",
         append: true,
         header: [
             {id: "id", title: "ID"},
@@ -250,24 +254,26 @@ const main = async () => {
         ]
     });
 
-    createReport("./corruptReport.txt", "CORRUPT FILES REPORT");
-    createReport("./checkingReport.txt", "CHECKING REPORT");
+    createReport("corruptReport.txt", "CORRUPT FILES REPORT");
+    createReport("checkingReport.txt", "CHECKING REPORT");
+    createReport("proofOfPossesionReport.txt", "PROOF OF POSSESION REPORT");
     // Corrupt a random file each randomTime seconds
-    let interval = setInterval(function () {
-        console.log("\n\nCorrupting files from " + filesPath);
+    let interval = setInterval(async function () {
+        //console.log("\n\nCorrupting files from " + filesPath);
         var textInterval1 = "\n\nCorrupting files from " + filesPath;
-        createReport("./corruptReport.txt", textInterval1);
-        const randomFilesNumber = randomNumber(1, keysArray.length);
+        createReport("corruptReport.txt", textInterval1);
+        const randomFilesNumber = randomNumber(1, keysArray.length-1);
         for (let i = 0; i < randomFilesNumber; i++) {
-            const file = filesPath + keysArray[randomNumber(0, keysArray.length)];
+            const file = filesPath + keysArray[randomNumber(0, keysArray.length-1)];
+            await new Promise(resolve => setTimeout(resolve, 10));
             corruptRandomFile(file);
         }
-    }, 995);
+    }, 1000);
 
     var index = 1;
     // Check integrity of each file each second
-    let interval2 = setInterval(function () {
-        console.log("\n\nChecking integrity of files from " + filesPath);
+    let interval2 = setInterval(async function () {
+        //console.log("\n\nChecking integrity of files from " + filesPath);
         var textInterval2 = "\n\nChecking integrity of files from " + filesPath;
         let data = [0,0];
         for (let i = 0; i < keysArray.length; i++) {
@@ -279,24 +285,27 @@ const main = async () => {
                 if (data[0] + data[1] == keysArray.length) {
                     createCSVReport(csv, index, data[0], data[1]);
                     index++;
-                    console.log("\n\nIntegrity check finished with the following results: \n" + data[0] + " files are OK\n" + data[1] + " files are corrupted\n");
+                    //console.log("\n\nIntegrity check finished with the following results: \n" + data[0] + " files are OK\n" + data[1] + " files are corrupted\n");
                     textInterval2 += "\n\nIntegrity check finished with the following results: \n" + data[0] + " files are OK\n" + data[1] + " files are corrupted\n";
-                    createReport("./checkingReport.txt", textInterval2);
+                    createReport("checkingReport.txt", textInterval2);
                 }
             });
 
             let nonce = createNonce();
             const fileHash = createHash(filesPath + file,nonce);
             const fileFullPath = filesPath + file;
-
+            await new Promise(resolve => setTimeout(resolve, 10));
+            createReport("proofOfPossesionReport.txt", `\n\nFile: ${fileFullPath}\nNonce: ${nonce}\nHash: ${fileHash}`);
 
             const pop = proofOfPossesion(fileFullPath,fileHash,""+3,nonce);
             if(pop.success){
-                console.log("HMAC is OK");
-                console.log("HMAC: " + pop.hmac);
+                //console.log("HMAC is OK");
+                //console.log("HMAC: " + pop.hmac);
+                createReport("proofOfPossesionReport.txt", `HMAC is OK\nHMAC: ${pop.hmac}`);
             }else {
-                console.log("HMAC is not OK");
-                console.log(pop.message);
+                //console.log("HMAC is not OK");
+                //console.log(pop.message);
+                createReport("proofOfPossesionReport.txt", `HMAC is not OK\n${pop.message}`);
             }
         }
     }, 1000);
@@ -307,7 +316,37 @@ const main = async () => {
     setTimeout(() => {
         clearInterval(interval);
         clearInterval(interval2);
-    }, 10000);
+    }, 30500);
+}
+
+//  main function
+const main = async () => {
+    // User inputs to select the type of hash and sending mail
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('¿Qué función hash quiere utilizar?\n0 => sha256\n1 => sha512\n2 => sha384\n', function (hashInputI) {
+        hashInput = hashInputI;
+        rl.question('¿Quieres que se te notifique por correo si se encuentra un archivo corrupto? [S/N]\n', function (sendMailYesNoInput) {
+            if (sendMailYesNoInput == "S") rl.question('¿Quieres que se te notifique por correo si se encuentra un archivo corrupto?', function (correoInput) {
+                sendMailYesNo = true;
+                correoDst = correoInput;
+                console.log(`Ah weno, que you choose ${hashInput} hash and you want to send a mail to ${correoDst}`);
+                rl.close();
+            });
+            else {
+                console.log(`Ah weno, que you choose ${hashInput} hash and you want to send a mail to ${correoDst}`);
+                rl.close();
+            }
+        });
+    });
+    
+    rl.on('close', function () {
+        serverSimulation();
+    });
+    
 }
 
 main();
