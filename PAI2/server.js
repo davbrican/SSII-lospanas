da0bd102686ccd16396a0c55e6ea6581bab2eb3d
davbrican <72500941+davbrican@users.noreply.github.com>
2022-03-22 13:14:35 +0100

    Update server.js

--- PAI2/server.js
+++ PAI2/server.js
@@ -5,16 +5,15 @@ const socket = new WebSocket('ws://localhost:8081');
 
 var nonces = [];
 const hashType = ['sha256', 'sha512', 'sha384'];
-var hashInput = 0;
 const secret = 'clave-simetrica-secreta';
 
+/*
 var object2send = {
     message: "34567891 987654 300",
     hmac: "",
     hashType: hashInput,
     nonce: null
 };
-
 
 // Function to create Nonce
 function createNonce() {
@@ -25,13 +24,14 @@ function createNonce() {
         return nonce;
     }
 }
+*/
 
 // Function to create HMAC
-function createHmac(message) {
+function createHmac(message, nonce, hashInput) {
     const hmac = crypto.createHmac(hashType[hashInput], secret);
     
-    const nonce = createNonce();
-    object2send.nonce = nonce;
+    //const nonce = createNonce();
+    //object2send.nonce = nonce;
 
     hmac.update(message + nonce);
 
@@ -48,9 +48,13 @@ wss.on('connection', function connection(ws) {
 
     // Wire up logic for the message event (when a client sends something)
     ws.on('message', function incoming(message) {
-        console.log('received: %s', JSON.parse(message));
-        // Send a message
-        ws.send(JSON.stringify(JSON.parse(message)));
+        objectReceived = JSON.parse(message);
+        console.log('received: %s', objectReceived);
+        if (createHmac(objectReceived.message, objectReceived.nonce, objectReceived.hashType) === objectReceived.hmac) {
+            ws.send(JSON.stringify({"message": "OK"}));
+        } else {
+            ws.send(JSON.stringify({"message": "HMAC incorrecto"}));
+        }
     });
 
 });
