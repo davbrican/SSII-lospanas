044c8a6f65e174cb3ae7189eda23f6efcf4b9cf1
JesusAparicioOrtiz <jesapaort@alum.us.es>
2022-03-26 12:21:34 +0100

    update

--- CAI-4/index.py
+++ CAI-4/index.py
@@ -7,15 +7,11 @@ message = b"hello world"
 
 print("Texto plano:", message)
 
-secret2 = "secret message2"
-
 key = os.urandom(32)
 cip = ChaCha20Poly1305(key)
 
 nonce = os.urandom(12)
 ciphertext = cip.encrypt(nonce, message)
-
-# plaintext = cip.decrypt(nonce, ciphertext)   #En caso de querer descrifrar el mensaje
 
 print("Texto cifrado:", ciphertext)
 
@@ -24,3 +20,13 @@ key2 = os.urandom(32)
 h = hmac.new( key2, ciphertext, hashlib.sha256 )
 hex = h.hexdigest()
 print("hmac:", hex)
+
+
+#verificación en el servidor
+
+h2 = hmac.new( key2, ciphertext, hashlib.sha256 )
+hex2 = h2.hexdigest()
+print('Verificación del mensaje:',hex2==hex)
+
+plaintext = cip.decrypt(nonce, ciphertext)   #En caso de querer descrifrar el mensaje
+print("Texto plano:", plaintext)
