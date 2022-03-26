b3d652b75d45995a447adfd1549d3137be9b0e79
davbrican <72500941+davbrican@users.noreply.github.com>
2022-03-26 12:31:31 +0100

    yepa

--- CAI-4/descripcionImagen.py
+++ CAI-4/descripcionImagen.py
@@ -1,4 +1,20 @@
 from stegano import exifHeader
-secret = exifHeader.hide("./tests/sample-files/20160505T130442.jpg",
-                        "./image.jpg", secret_message="Hello world!")
-print(exifHeader.reveal("./image.jpg"))
+from stegano.lsbset import generators
+import os
+from chacha20poly1305 import ChaCha20Poly1305
+
+
+secret_message = b'Hello World!'
+key = os.urandom(32)
+cip = ChaCha20Poly1305(key)
+
+nonce = os.urandom(12)
+ciphertext = cip.encrypt(nonce, secret_message)
+
+secret = exifHeader.hide("./entrada.jpeg",
+                        "./resultado.bmp", secret_message=secret_message)
+
+message = exifHeader.reveal("./resultado.bmp")
+
+
+print(message)
